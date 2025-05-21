4ba979a76c5a4c81cf6a2c4021b014acb1ab2ffe
m4nuul <nikitakortrlr020@gmail.com>
2025-05-21 16:12:44 +0700

    added pocklington test

--- task2.cpp
+++ task2.cpp
@@ -5,12 +5,5 @@ using namespace std;
 
 int main()
 {
-   int n;
-   int target;
-   cout << "Enter digit to test: ";
-   cin >> n;
-   cout << "Enter target digit: ";
-   cin >> target;
-   int Miller_test_result = Miller_test(n, target);
    return 0;
 }
